206f0a3a79edd748723c3dacbed1948aa75a4578
Dheeraj Singh <dheeraj26singh@gmail.com>
2022-02-11 22:10:20 +0530

    v2-beta

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-import re
-from flask import *
-import qrcode
+from flask import Flask, render_template, redirect, request, send_file, session, url_for
 from pytube import YouTube
+from dotenv import load_dotenv
+from zenora import APIClient
 from src.speedtester import speed
 from src.downloader import ytdownload
+from src.database import PlayerDataBase
+import qrcode
+import os
+
+load_dotenv()
+
+token = os.environ.get('TOKEN')
+client_secret = os.environ.get('CLIENT_SECRET')
+redirect_uri = os.environ.get('REDIRECT_URI')
+oauth_uri = os.environ.get('OAUTH_URI')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'IndominusRexian'
+client = APIClient(token=token, client_secret=client_secret)
+
 
 @app.route("/")
 def home():
-    return render_template("home.html",template_folder="templates")
+    if 'token' in session:
+        bearer_client = APIClient(session['token'], bearer=True)
+        user = bearer_client.users.get_current_user()         
+        return render_template("home.html", details=user)
+    return render_template("home.html",)
 
 @app.route("/test/")
 def test():
@@ -23,7 +40,7 @@ def qrcreate():
         qr.save("qr_code.png")
         filename = "qr_code.png"
         return send_file(filename, mimetype='image/gif')
-    return render_template("qrcreate.html",template_folder="templates")
+    return render_template("qrcreate.html",)
 
 @app.route("/downloader/",methods = ['GET', 'POST'])
 def downloader():
@@ -31,8 +48,8 @@ def downloader():
         yt = ytdownload()
         link = YouTube(request.form.get("ytlink"))
         filepath = yt.download(link)
-        return render_template("video_watch.html",template_folder="templates", filetitle=link.title, filepath= filepath, thumbnail= link.thumbnail_url)
-    return render_template("downloader.html", template_folder="templates")    
+        return render_template("video_watch.html", filetitle=link.title, filepath= filepath, thumbnail= link.thumbnail_url)
+    return render_template("downloader.html", )    
 
 @app.route("/download/",methods = ['GET', 'POST'])
 def download():
@@ -48,19 +65,110 @@ def speedtesting():
 
 @app.route("/redirect/discord")
 def redirect_discord():
-    return render_template("discord_redirect.html", template_folder="templates")
+    return render_template("discord_redirect.html", )
 
 @app.route("/soon")
 def soon():
-    return render_template("soon.html", template_folder="templates")
+    return render_template("soon.html", )
 
 @app.route("/404")
 def invalid_route_404():
-    return render_template("404.html", template_folder="templates")
+    return render_template("404.html", )
 
 @app.errorhandler(404) 
 def invalid_route(e): 
     return render_template("404.html",templates_folder="templates")
+
+@app.route('/login')
+def login():
+    if 'token' in session:
+        bearer_client = APIClient(session['token'], bearer=True)
+        user = bearer_client.users.get_current_user()
+        return redirect('/profile')  
+    return render_template('login.html', oauth_uri=oauth_uri)
+
+@app.route('/oauth/callback')
+def callback():
+    try:
+        code = request.args['code']
+        access_token = client.oauth.get_access_token(code, redirect_uri).access_token
+        session['token'] = access_token
+        return redirect('/oauth/callback/gamename')
+    except:
+        return redirect('/login')
+
+@app.route('/oauth/callback/gamename', methods=['GET', 'POST'])
+def gamename(): 
+    if 'token' in session:
+        bearer_client = APIClient(session['token'], bearer=True)
+        user = bearer_client.users.get_current_user()  
+        db = PlayerDataBase(user.id)
+        details = db.get_account()
+        if details == False:
+            if request.method == 'POST':
+                gname = request.form.get('gname')
+                db = PlayerDataBase(user.id)
+                acc = db.create_account(gname)
+                if acc == True: #Account has been created 
+                    return redirect('/profile/redirect')
+                else: #Account is already there.
+                    return redirect('/login/redirect')  
+        else:
+            if user.id == details['discord_id']:
+                return redirect('/profile')                          
+        return render_template('gamename.html', user=user)
+    return redirect('/login')    
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
+@app.route('/profile')
+def profile():
+    if 'token' in session:
+        bearer_client = APIClient(session['token'], bearer=True)
+        user = bearer_client.users.get_current_user()          
+        db = PlayerDataBase(user.id)
+        details = db.get_account()
+        return render_template('profile.html', show_details=True, title=details['game_name'], details=details, user=user)
+    else:
+        return redirect('/login')    
+
+@app.route('/profile/redirect')
+def profile_redirect():
+    if 'token' in session:
+        bearer_client = APIClient(session['token'], bearer=True)
+        user = bearer_client.users.get_current_user()          
+        db = PlayerDataBase(user.id)
+        details = db.get_account()        
+        return render_template('profile.html', redirect_success=True, show_details=True, title=details['game_name'], details=details, user=user)
+    else:
+        return redirect('/login')
+
+@app.route('/addchamp', methods=['GET', 'POST'])
+def champ_adder():
+    if 'token' in session:
+        bearer_client = APIClient(session['token'], bearer=True)
+        user = bearer_client.users.get_current_user()          
+        db = PlayerDataBase(user.id)
+        details = db.get_account()  
+        champ_names = db.get_fancy_champ_names()
+        if 'POST' in request.method:
+            champname = db.fancy_to_standard(request.form.get('champ-name'))
+            print(champname)
+            if champname != False:
+                tier = int(request.form.get('champ-tier'))
+                rank = int(request.form.get('champ-rank'))
+                signature = int(request.form.get('champ-sig'))
+                db.add_champ(champname, tier, rank, signature)
+                if db.error != None:
+                    return render_template('addchamp.html', error_status=True, error=db.error, title = user.username, details=details, champ_names=champ_names)
+                elif db.details != None:
+                    return render_template('addchamp.html', error_status=False, detail=db.details, title = user.username, details=details, champ_names=champ_names)    
+        return render_template('addchamp.html', details=details, champ_names=champ_names, sigs=range(0,201), title = user.username)
+    else:
+        return redirect('/login')
 
 
 if __name__ =='__main__':
--- src/database.py
+++ src/database.py
@@ -10,10 +10,13 @@ class PlayerDataBase():
     '''
     def __init__(self, discord_id):
         self.id = discord_id
-        cluster_code = os.environ.get('cluster')
+        cluster_code = os.environ.get('CLUSTER')
         cluster = MongoClient(cluster_code)
+        self.cluster = cluster
         self.db = cluster["MCOC"]["Account"]
         self.user_acc = self.db.find_one({'discord_id':discord_id}, {'_id':0})
+        self.error = None
+        self.details = None
         
     def check_acc_exist(self):
         '''
@@ -25,6 +28,9 @@ class PlayerDataBase():
             return(True) #Account exists and/or details are correct.
 
     def create_account(self, game_name):
+        '''
+        Creates an account in the DB. Returns False if the account wasn't made. 
+        '''
         if self.user_acc is None:
             acc_dict = {"game_name":game_name,
                         "discord_id":self.id,
@@ -34,3 +40,101 @@ class PlayerDataBase():
             return(True) #Account was created.
         else:
             return(False) #Account wasn't made as it already exists.
+
+    def get_account(self): 
+        '''
+        Gives account details. If account details weren't found, False is returned.
+        '''        
+        if self.user_acc is not None:
+            return(self.user_acc)
+        else:
+            return(False)  
+    
+
+    def get_fancy_champ_names(self):
+        champ_name_list = ['ABOMINATION', 'ABOMINATION (IMMORTAL)', 'ÆGON', 'AGENT VENOM', 'AIR-WALKER', 'AMERICA CHAVEZ', 'ANGELA', 'ANNIHILUS', 'ANT-MAN', 'ANTI-VENOM', 'APOCALYPSE', 'ARCHANGEL', 'BEAST', 'BISHOP', 'BLACK BOLT', 'BLACK PANTHER', 'BLACK PANTHER (CIVIL WAR)', 'BLACK WIDOW', 'BLACK WIDOW (CLAIRE VOYANT)', 'BLACK WIDOW (DEADLY ORIGIN)', 'BLADE', 'CABLE', 'CAPTAIN AMERICA', 'CAPTAIN AMERICA (INFINITY WAR)', 'CAPTAIN AMERICA (SAM WILSON)', 'CAPTAIN AMERICA (WWII)', 'CAPTAIN MARVEL', 'CAPTAIN MARVEL (CLASSIC)', 'CARNAGE', 'CIVIL WARRIOR', 'COLOSSUS', 'CORVUS GLAIVE', 'COSMIC GHOST RIDER', 'CROSSBONES', 'CULL OBSIDIAN', 'CYCLOPS (BLUE TEAM)', 'CYCLOPS (NEW XAVIER SCHOOL)', 'DAREDEVIL (CLASSIC)', "DAREDEVIL (HELL'S KITCHEN)", 'DARKHAWK', 'DEADPOOL', 'DEADPOOL (X-FORCE)', 'DIABLO', 'DOCTOR DOOM', 'DOCTOR OCTOPUS', 'DOCTOR STRANGE', 'DOCTOR VOODOO', 'DOMINO', 'DORMAMMU', 'DRAGON MAN', 'DRAX', 'EBONY MAW', 'ELECTRO', 'ELEKTRA', 'ELSA BLOODSTONE', 'EMMA FROST', 'FALCON', 'GAMBIT', 'GAMORA', 'GHOST', 'GHOST RIDER', 'GOLDPOOL', 'GREEN GOBLIN', 'GROOT', 'GUARDIAN', 'GUILLOTINE', 'GUILLOTINE 2099', 'GWENPOOL', 'HAVOK', 'HAWKEYE', 'HEIMDALL', 'HELA', 'HERCULES', 'HIT-MONKEY', 'HOWARD THE DUCK', 'HULK', 'HULK (IMMORTAL)', 'HULK (RAGNAROK)', 'HULKBUSTER', 'HUMAN TORCH', 'HYPERION', 'ICEMAN', 'IKARIS', 'INVISIBLE WOMAN', 'IRON FIST', 'IRON FIST (IMMORTAL)', 'IRON MAN', 'IRON MAN (INFINITY WAR)', 'IRON PATRIOT', 'JABARI PANTHER', 'JOE FIXIT', 'JUBILEE', 'JUGGERNAUT', 'KANG', 'KARNAK', 'KILLMONGER', 'KING GROOT', 'KINGPIN', 'KITTY PRYDE', 'KNULL', 'KORG', 'KRAVEN', 'LOKI', 'LONGSHOT', 'LUKE CAGE', 'MISTY KNIGHT', 'M.O.D.O.K.', 'MAGIK', 'MAGNETO', 'MAGNETO (HOUSE OF X)', 'MAN-THING', 'MANGOG', 'MASACRE', 'MEDUSA', 'MEPHISTO', 'MISTER FANTASTIC', 'MISTER NEGATIVE', 'MISTER SINISTER', 'MOJO', 'MOLE MAN', 'MOON KNIGHT', 'MORDO', 'MORNINGSTAR', 'MS. MARVEL', 'MS. MARVEL (KAMALA KHAN)', 'MYSTERIO', 'NAMOR', 'NEBULA', 'NICK FURY', 'NIGHT THRASHER', 'NIGHTCRAWLER', 'NIMROD', 'NOVA', 'ODIN', 'OLD MAN LOGAN', 'OMEGA RED', 'PENI PARKER', 'PHOENIX', 'PLATINUMPOOL', 'PROFESSOR X', 'PROXIMA MIDNIGHT', 'PSYCHO-MAN', 'PSYLOCKE', 'PUNISHER', 'PUNISHER 2099', 'PURGATORY', 'QUAKE', 'RED GOBLIN', 'RED GUARDIAN', 'RED HULK', 'RED SKULL', 'RHINO', 'ROCKET RACCOON', 'ROGUE', 'RONAN', 'RONIN', 'SABRETOOTH', 'SASQUATCH', 'SAURON', 'SCARLET WITCH', 'SCARLET WITCH (CLASSIC)', 'SENTINEL', 'SENTRY', 'SERSI', 'SHANG-CHI', 'SHE-HULK', 'SILVER CENTURION', 'SILVER SURFER', 'SORCERER SUPREME', 'SPIDER-GWEN', 'SPIDER-HAM', 'SPIDER-MAN (CLASSIC)', 'SPIDER-MAN (MILES MORALES)', 'SPIDER-MAN (STARK ENHANCED)', 'SPIDER-MAN (STEALTH SUIT)', 'SPIDER-MAN (SYMBIOTE)', 'SPIDER-MAN 2099', 'SQUIRREL GIRL', 'STAR-LORD', 'STORM', 'STORM (PYRAMID X)', 'STRYFE', 'SUNSPOT', 'SUPER-SKRULL', 'SUPERIOR IRON MAN', 'SYMBIOTE SUPREME', 'TASKMASTER', 'TERRAX', 'THANOS', 'THE CHAMPION', 'THE HOOD', 'THE OVERSEER', 'THING', 'THOR', 'THOR (JANE FOSTER)', 'THOR (RAGNAROK)', 'TIGRA', 'TOAD', 'ULTRON', 'ULTRON', 'UNSTOPPABLE COLOSSUS', 'VENOM', 'VENOM THE DUCK', 'VENOMPOOL', 'VISION', 'VISION (AARKUS)', 'VISION (AGE OF ULTRON)', 'VOID', 'VULTURE', 'WAR MACHINE', 'WARLOCK', 'WASP', 'WINTER SOLDIER', 'WOLVERINE', 'WOLVERINE (WEAPON X)', 'WOLVERINE (X-23)', 'YELLOWJACKET', 'YONDU']
+        return champ_name_list
+
+    def get_champ_names(self):
+        champs_list = ['abomination', 'abomination_immortal', 'aegon', 'agentvenom', 'airwalker', 'americachavez', 'angela', 'annihilus', 'antman', 'antivenom', 'apocalypse', 'archangel', 'beast', 'bishop', 'blackbolt', 'blackpanther', 'blackpanther_cw', 'blackwidow', 'blackwidow_timely', 'blackwidow_movie', 'blade', 'cable', 'captainamerica', 'captainamerica_movie', 'captainamerica_samwilson' ,'captainamerica_ww2', 'captainmarvel_movie', 'captainmarvel', 'carnage', 'civilwarrior', 'colossus', 'corvusglaive', 'ghostrider_cosmic', 'crossbones', 'cullobsidian', 'cyclops_90s', 'cyclops', 'daredevil', 'daredevil_netflix', 'darkhawk', 'deadpool', 'deadpool_xforce', 'diablo', 'doctordoom', 'doc_ock', 'drstrange', 'brothervoodoo', 'domino', 'dormammu', 'dragonman', 'drax', 'ebonymaw', 'electro', 'elektra', 'elsabloodstone', 'emmafrost', 'falcon', 'gambit', 'gamora', 'ghost', 'ghostrider', 'deadpool_gold', 'green_goblin', 'groot', 'guardian', 'guillotine', 'guillotine_2099', 'gwenpool', 'havok', 'hawkeye', 'heimdall', 'hela', 'hercules', 'hitmonkey', 'howardmech', 'hulk', 'hulk_immortal', 'hulk_ragnarok', 'hulkbuster_movie', 'humantorch', 'hyperion', 'iceman', 'ikaris', 'invisiblewoman', 'ironfist', 'ironfist_white', 'ironman', 'ironman_movie', 'ironpatriot', 'blackpanther_realm', 'joefixit', 'jubilee', 'juggernaut', 'kang', 'karnak', 'killmonger', 'groot_king', 'kingpin', 'kittypryde', 'knull', 'korg', 'kraven', 'loki', 'longshot', 'lukecage', 'mistyknight' ,'modok', 'magik', 'magneto', 'magneto_marvelnow', 'manthing', 'mangog', 'masacre', 'medusa', 'mephisto', 'mrfantastic', 'misternegative', 'mistersinister', 'mojo', 'moleman', 'moonknight', 'karlmordo', 'morningstar', 'msmarvel', 'msmarvel_kamala', 'mysterio', 'namor', 'nebula', 'nickfury', 'nightthrasher', 'nightcrawler', 'nimrod', 'nova', 'odin', 'wolverine_oldman', 'omegared', 'peniparker', 'phoenix', 'deadpool_platinum', 'professorx', 'proximamidnight', 'psychoman', 'psylocke', 'punisher', 'punisher_2099', 'purgatory', 'quake', 'red_goblin', 'redguardian', 'hulk_red', 'redskull', 'rhino', 'rocket', 'rogue', 'ronan', 'ronin', 'sabretooth', 'sasquatch', 'sauron', 'scarletwitch_current', 'scarletwitch', 'sentinel', 'sentry', 'sersi', 'shangchi', 'shehulk', 'ironman_silvercenturion', 'silversurfer', 'drstrange_realm', 'spidergwen', 'spiderham', 'spiderman', 'spiderman_morales', 'spiderman_movie', 'spiderman_stealth', 'spiderman_black', 'spiderman_2099', 'squirrelgirl', 'starlord', 'storm', 'storm_realm', 'stryfe', 'sunspot', 'skrull_super', 'ironman_superior', 'symbiotesupreme', 'taskmaster', 'terrax', 'thanos', 'champion', 'hood', 'maestro_overseer', 'thing', 'thor', 'thor_janefoster', 'thor_ragnarok', 'tigra', 'toad', 'ultron', 'ultron_prime', 'colossus_unstoppable', 'venom', 'venomtheduck', 'venompool', 'vision', 'vision_timely', 'vision_movie', 'void', 'vulture_movie', 'warmachine', 'warlock', 'wasp', 'wintersoldier', 'wolverine', 'wolverine_weaponx', 'x23', 'yellowjacket', 'yondu']
+        return champs_list
+
+    def fancy_to_standard(self, champname):
+        standard_champs_list = ['abomination', 'abomination_immortal', 'aegon', 'agentvenom', 'airwalker', 'americachavez', 'angela', 'annihilus', 'antman', 'antivenom', 'apocalypse', 'archangel', 'beast', 'bishop', 'blackbolt', 'blackpanther', 'blackpanther_cw', 'blackwidow', 'blackwidow_timely', 'blackwidow_movie', 'blade', 'cable', 'captainamerica', 'captainamerica_movie', 'captainamerica_samwilson' ,'captainamerica_ww2', 'captainmarvel_movie', 'captainmarvel', 'carnage', 'civilwarrior', 'colossus', 'corvusglaive', 'ghostrider_cosmic', 'crossbones', 'cullobsidian', 'cyclops_90s', 'cyclops', 'daredevil', 'daredevil_netflix', 'darkhawk', 'deadpool', 'deadpool_xforce', 'diablo', 'doctordoom', 'doc_ock', 'drstrange', 'brothervoodoo', 'domino', 'dormammu', 'dragonman', 'drax', 'ebonymaw', 'electro', 'elektra', 'elsabloodstone', 'emmafrost', 'falcon', 'gambit', 'gamora', 'ghost', 'ghostrider', 'deadpool_gold', 'green_goblin', 'groot', 'guardian', 'guillotine', 'guillotine_2099', 'gwenpool', 'havok', 'hawkeye', 'heimdall', 'hela', 'hercules', 'hitmonkey', 'howardmech', 'hulk', 'hulk_immortal', 'hulk_ragnarok', 'hulkbuster_movie', 'humantorch', 'hyperion', 'iceman', 'ikaris', 'invisiblewoman', 'ironfist', 'ironfist_white', 'ironman', 'ironman_movie', 'ironpatriot', 'blackpanther_realm', 'joefixit', 'jubilee', 'juggernaut', 'kang', 'karnak', 'killmonger', 'groot_king', 'kingpin', 'kittypryde', 'knull', 'korg', 'kraven', 'loki', 'longshot', 'lukecage', 'mistyknight' ,'modok', 'magik', 'magneto', 'magneto_marvelnow', 'manthing', 'mangog', 'masacre', 'medusa', 'mephisto', 'mrfantastic', 'misternegative', 'mistersinister', 'mojo', 'moleman', 'moonknight', 'karlmordo', 'morningstar', 'msmarvel', 'msmarvel_kamala', 'mysterio', 'namor', 'nebula', 'nickfury', 'nightthrasher', 'nightcrawler', 'nimrod', 'nova', 'odin', 'wolverine_oldman', 'omegared', 'peniparker', 'phoenix', 'deadpool_platinum', 'professorx', 'proximamidnight', 'psychoman', 'psylocke', 'punisher', 'punisher_2099', 'purgatory', 'quake', 'red_goblin', 'redguardian', 'hulk_red', 'redskull', 'rhino', 'rocket', 'rogue', 'ronan', 'ronin', 'sabretooth', 'sasquatch', 'sauron', 'scarletwitch_current', 'scarletwitch', 'sentinel', 'sentry', 'sersi', 'shangchi', 'shehulk', 'ironman_silvercenturion', 'silversurfer', 'drstrange_realm', 'spidergwen', 'spiderham', 'spiderman', 'spiderman_morales', 'spiderman_movie', 'spiderman_stealth', 'spiderman_black', 'spiderman_2099', 'squirrelgirl', 'starlord', 'storm', 'storm_realm', 'stryfe', 'sunspot', 'skrull_super', 'ironman_superior', 'symbiotesupreme', 'taskmaster', 'terrax', 'thanos', 'champion', 'hood', 'maestro_overseer', 'thing', 'thor', 'thor_janefoster', 'thor_ragnarok', 'tigra', 'toad', 'ultron', 'ultron_prime', 'colossus_unstoppable', 'venom', 'venomtheduck', 'venompool', 'vision', 'vision_timely', 'vision_movie', 'void', 'vulture_movie', 'warmachine', 'warlock', 'wasp', 'wintersoldier', 'wolverine', 'wolverine_weaponx', 'x23', 'yellowjacket', 'yondu']
+        fancy_champs_list = ['ABOMINATION', 'ABOMINATION (IMMORTAL)', 'ÆGON', 'AGENT VENOM', 'AIR-WALKER', 'AMERICA CHAVEZ', 'ANGELA', 'ANNIHILUS', 'ANT-MAN', 'ANTI-VENOM', 'APOCALYPSE', 'ARCHANGEL', 'BEAST', 'BISHOP', 'BLACK BOLT', 'BLACK PANTHER', 'BLACK PANTHER (CIVIL WAR)', 'BLACK WIDOW', 'BLACK WIDOW (CLAIRE VOYANT)', 'BLACK WIDOW (DEADLY ORIGIN)', 'BLADE', 'CABLE', 'CAPTAIN AMERICA', 'CAPTAIN AMERICA (INFINITY WAR)', 'CAPTAIN AMERICA (SAM WILSON)', 'CAPTAIN AMERICA (WWII)', 'CAPTAIN MARVEL', 'CAPTAIN MARVEL (CLASSIC)', 'CARNAGE', 'CIVIL WARRIOR', 'COLOSSUS', 'CORVUS GLAIVE', 'COSMIC GHOST RIDER', 'CROSSBONES', 'CULL OBSIDIAN', 'CYCLOPS (BLUE TEAM)', 'CYCLOPS (NEW XAVIER SCHOOL)', 'DAREDEVIL (CLASSIC)', "DAREDEVIL (HELL'S KITCHEN)", 'DARKHAWK', 'DEADPOOL', 'DEADPOOL (X-FORCE)', 'DIABLO', 'DOCTOR DOOM', 'DOCTOR OCTOPUS', 'DOCTOR STRANGE', 'DOCTOR VOODOO', 'DOMINO', 'DORMAMMU', 'DRAGON MAN', 'DRAX', 'EBONY MAW', 'ELECTRO', 'ELEKTRA', 'ELSA BLOODSTONE', 'EMMA FROST', 'FALCON', 'GAMBIT', 'GAMORA', 'GHOST', 'GHOST RIDER', 'GOLDPOOL', 'GREEN GOBLIN', 'GROOT', 'GUARDIAN', 'GUILLOTINE', 'GUILLOTINE 2099', 'GWENPOOL', 'HAVOK', 'HAWKEYE', 'HEIMDALL', 'HELA', 'HERCULES', 'HIT-MONKEY', 'HOWARD THE DUCK', 'HULK', 'HULK (IMMORTAL)', 'HULK (RAGNAROK)', 'HULKBUSTER', 'HUMAN TORCH', 'HYPERION', 'ICEMAN', 'IKARIS', 'INVISIBLE WOMAN', 'IRON FIST', 'IRON FIST (IMMORTAL)', 'IRON MAN', 'IRON MAN (INFINITY WAR)', 'IRON PATRIOT', 'JABARI PANTHER', 'JOE FIXIT', 'JUBILEE', 'JUGGERNAUT', 'KANG', 'KARNAK', 'KILLMONGER', 'KING GROOT', 'KINGPIN', 'KITTY PRYDE', 'KNULL', 'KORG', 'KRAVEN', 'LOKI', 'LONGSHOT', 'LUKE CAGE', 'MISTY KNIGHT', 'M.O.D.O.K.', 'MAGIK', 'MAGNETO', 'MAGNETO (HOUSE OF X)', 'MAN-THING', 'MANGOG', 'MASACRE', 'MEDUSA', 'MEPHISTO', 'MISTER FANTASTIC', 'MISTER NEGATIVE', 'MISTER SINISTER', 'MOJO', 'MOLE MAN', 'MOON KNIGHT', 'MORDO', 'MORNINGSTAR', 'MS. MARVEL', 'MS. MARVEL (KAMALA KHAN)', 'MYSTERIO', 'NAMOR', 'NEBULA', 'NICK FURY', 'NIGHT THRASHER', 'NIGHTCRAWLER', 'NIMROD', 'NOVA', 'ODIN', 'OLD MAN LOGAN', 'OMEGA RED', 'PENI PARKER', 'PHOENIX', 'PLATINUMPOOL', 'PROFESSOR X', 'PROXIMA MIDNIGHT', 'PSYCHO-MAN', 'PSYLOCKE', 'PUNISHER', 'PUNISHER 2099', 'PURGATORY', 'QUAKE', 'RED GOBLIN', 'RED GUARDIAN', 'RED HULK', 'RED SKULL', 'RHINO', 'ROCKET RACCOON', 'ROGUE', 'RONAN', 'RONIN', 'SABRETOOTH', 'SASQUATCH', 'SAURON', 'SCARLET WITCH', 'SCARLET WITCH (CLASSIC)', 'SENTINEL', 'SENTRY', 'SERSI', 'SHANG-CHI', 'SHE-HULK', 'SILVER CENTURION', 'SILVER SURFER', 'SORCERER SUPREME', 'SPIDER-GWEN', 'SPIDER-HAM', 'SPIDER-MAN (CLASSIC)', 'SPIDER-MAN (MILES MORALES)', 'SPIDER-MAN (STARK ENHANCED)', 'SPIDER-MAN (STEALTH SUIT)', 'SPIDER-MAN (SYMBIOTE)', 'SPIDER-MAN 2099', 'SQUIRREL GIRL', 'STAR-LORD', 'STORM', 'STORM (PYRAMID X)', 'STRYFE', 'SUNSPOT', 'SUPER-SKRULL', 'SUPERIOR IRON MAN', 'SYMBIOTE SUPREME', 'TASKMASTER', 'TERRAX', 'THANOS', 'THE CHAMPION', 'THE HOOD', 'THE OVERSEER', 'THING', 'THOR', 'THOR (JANE FOSTER)', 'THOR (RAGNAROK)', 'TIGRA', 'TOAD', 'ULTRON', 'ULTRON', 'UNSTOPPABLE COLOSSUS', 'VENOM', 'VENOM THE DUCK', 'VENOMPOOL', 'VISION', 'VISION (AARKUS)', 'VISION (AGE OF ULTRON)', 'VOID', 'VULTURE', 'WAR MACHINE', 'WARLOCK', 'WASP', 'WINTER SOLDIER', 'WOLVERINE', 'WOLVERINE (WEAPON X)', 'WOLVERINE (X-23)', 'YELLOWJACKET', 'YONDU']
+        try:
+            fancy_index = fancy_champs_list.index(champname)
+            standard_name = standard_champs_list[fancy_index]
+            return standard_name
+        except:
+            return False    
+
+    def add_champ(self,champname, tier, rank, signature):
+        self.db = self.cluster["MCOC"]["Account"]  
+        infodb = self.cluster['MCOC']['Champs']
+        gamename = self.user_acc['game_name']
+        player_check = self.db.find_one({"game_name":gamename}, {'_id': 0}) 
+        champ_details = infodb.find_one({'champid':champname}, {'_id': 0})
+        create_new = True  
+        if player_check is not None:
+            for roster_dict in player_check['roster']:
+                if tier == 6 and signature >200 and rank >4:
+                    self.error = 'Signature for a 6 star should not be above than 200 or/and rank of a 6 star should not be above than 4.'
+                elif tier == 5 and signature >200 and rank>5: 
+                    self.error = 'Signature for a 5 star should not be above than 200 or/and rank of a 5 star should not be above than 5.'  
+                elif tier == 4 and signature >99:
+                    self.error = 'Signature for a 4 star should not be above than 99 or/and rank of a 4 star should not be above than 5.'   
+                elif tier == 3 and signature >99 and rank>4:
+                    self.error = 'Signature for a 3 star should not be above than 99 or/and rank of a 3 star should not be above than 4.' 
+                elif tier == 2 and signature>99 and rank>3: 
+                    self.error = 'Signature for a 2 star should not be above than 99 or/and rank of a 2 star should not be above than 3.' 
+                elif tier == 1:
+                    self.error = '2 star and 1 star champs are not supported.'
+                else:                 
+                    if roster_dict['champ_name'] == champ_details['data'][f'{tier}+{rank}']['name'] and roster_dict['tier'] == int(tier):
+                        create_new = False
+                        print(create_new)
+                        break            
+            if champ_details is not None:
+                if create_new == True:
+                    if tier == 6 and signature >200 and rank >4:
+                        self.error = 'Signature for a 6 star should not be above than 200 or/and rank of a 6 star should not be above than 4.'
+                    elif tier == 5 and signature >200 and rank>5: 
+                        self.error = 'Signature for a 5 star should not be above than 200 or/and rank of a 5 star should not be above than 5.'  
+                    elif tier == 4 and signature >99:
+                        self.error = 'Signature for a 4 star should not be above than 99 or/and rank of a 4 star should not be above than 5.'   
+                    elif tier == 3 and signature >99 and rank>4:
+                        self.error = 'Signature for a 3 star should not be above than 99 or/and rank of a 3 star should not be above than 4.' 
+                    elif tier == 2 and signature>99 and rank>3: 
+                        self.error = 'Signature for a 2 star should not be above than 99 or/and rank of a 2 star should not be above than 3.' 
+                    elif tier == 1:
+                        self.error = '2 star and 1 star champs are not supported.'
+                    else:                                             
+                        champ_info = {
+                            'champ_name':champ_details['data'][f'{tier}+{rank}']['name'],
+                            'tier': tier,
+                            'rank': rank,
+                            'prestige':champ_details['data'][f'{tier}+{rank}']['prestige'],
+                            'sig_number':signature,
+                            'img_link':champ_details['data'][f'{tier}+{rank}']['img_potrait'],
+                            'champid':f'{champ_details["champid"]}+{signature}+{tier}+{rank}',
+                            'url_page':champ_details['data'][f'{tier}+{rank}']['url_page']
+                        }
+                        player_check['roster'].append(champ_info)
+                        self.db.find_one_and_replace({"game_name":gamename}, player_check)
+                        self.details = f"Added {tier} star {champ_details['data'][f'{tier}+{rank}']['name']} of rank {rank}, tier {tier} and signature {signature}"
+                else:
+                    #Updates the signature and champid
+                    for roster_dict in player_check['roster']:
+                        if roster_dict['champ_name'] == champ_details['data'][f'{tier}+{rank}']['name'] and roster_dict['tier'] == int(tier): 
+                            self.details = f"Updated {tier} star {champ_details['data'][f'{tier}+{rank}']['name']} of previous signature {roster_dict['sig_number']} to new signature {signature} and previous rank {roster_dict['rank']} to new rank {rank}"
+                            roster_dict['sig_number'] = signature
+                            roster_dict['rank'] = rank
+                            roster_dict['champid'] = f'{champ_details["champid"]}+{signature}'
+                            break        
+                    self.db.find_one_and_replace({"game_name":gamename}, player_check)                         
+            else:
+                self.error = 'Champname is incorrect. Refer to https://github.com/Rexians/uma/blob/master/champnames.md for correct champnames.'    
+        else:
+            self.error = 'Player with this name doesn\'t exists.'                                  
+
